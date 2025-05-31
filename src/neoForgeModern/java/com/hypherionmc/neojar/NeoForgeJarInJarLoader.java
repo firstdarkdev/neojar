373da1b4c1993b89ebc920bd7d271b31785c03ca
HypherionSA <hypherionmc@gmail.com>
2025-05-31 19:18:08 +0200

    More Loader work

--- src/neoForgeModern/java/com/hypherionmc/neojar/NeoForgeJarInJarLoader.java
+++ src/neoForgeModern/java/com/hypherionmc/neojar/NeoForgeJarInJarLoader.java
@@ -2,17 +2,17 @@ package com.hypherionmc.neojar;
 
 import com.google.common.collect.ImmutableMap;
 import com.mojang.logging.LogUtils;
+import cpw.mods.jarhandling.JarContents;
 import cpw.mods.jarhandling.SecureJar;
-import net.neoforged.fml.loading.moddiscovery.AbstractJarFileDependencyLocator;
-import net.neoforged.fml.loading.moddiscovery.ModFile;
+import net.neoforged.neoforgespi.locating.IDependencyLocator;
+import net.neoforged.neoforgespi.locating.IDiscoveryPipeline;
 import net.neoforged.neoforgespi.locating.IModFile;
-import org.apache.commons.io.FileUtils;
+import net.neoforged.neoforgespi.locating.ModFileDiscoveryAttributes;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 
 import java.io.InputStream;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -20,7 +20,6 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -29,16 +28,16 @@ import java.util.Optional;
  * A Custom JarInJar Loader for NeoForge.
  */
 @SuppressWarnings("unused")
-public class NeoForgeJarInJarLoader extends AbstractJarFileDependencyLocator {
+public class NeoForgeJarInJarLoader implements IDependencyLocator {
 
     private static final Logger LOGGER = LogUtils.getLogger();
 
     @Override
-    public List<IModFile> scanMods(Iterable<IModFile> loadedMods) {
+    public void scanMods(List<IModFile> list, IDiscoveryPipeline iDiscoveryPipeline) {
         try {
             // Create a fake SecureJar and ModFile for the loader
             SecureJar secureJar = SecureJar.from(Path.of(NeoForgeJarInJarLoader.class.getProtectionDomain().getCodeSource().getLocation().toURI()));
-            IModFile modFile = new ModFile(secureJar, this, (iMod) -> null);
+            IModFile modFile = IModFile.create(secureJar, (iMod) -> null);
 
             // Eventual list of JIJ jars to load, if any
             List<IModFile> modFiles = new ArrayList<>();
@@ -52,22 +51,22 @@ public class NeoForgeJarInJarLoader extends AbstractJarFileDependencyLocator {
                 boolean hasSharedLibrary = Boolean.parseBoolean(orionData[2]);
 
                 // Load the NeoForge mod jar
-                loadModFromJar(modFile, String.format("META-INF/orion/%s-neoforge-%s.jar", modId, modVersion), false).ifPresent(modFiles::add);
+                loadModFromJar(modFile, String.format("META-INF/orion/%s-neoforge-%s.jar", modId, modVersion), false, iDiscoveryPipeline).ifPresent(modFiles::add);
 
                 // Load the Shared Library (de-duplicated classes and resources), if there is one
                 if (hasSharedLibrary) {
-                    loadModFromJar(modFile, String.format("META-INF/orion/%s-shared-%s.jar", modId, modVersion), true).ifPresent(modFiles::add);
+                    loadModFromJar(modFile, String.format("META-INF/orion/%s-shared-%s.jar", modId, modVersion), true, iDiscoveryPipeline).ifPresent(modFiles::add);
                 }
             }
 
-            return modFiles;
+            modFiles.forEach(iDiscoveryPipeline::addModFile);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @SuppressWarnings("resource")
-    protected Optional<IModFile> loadModFromJar(IModFile file, String path, boolean isSharedLibrary) {
+    protected Optional<IModFile> loadModFromJar(IModFile file, String path, boolean isSharedLibrary, IDiscoveryPipeline iDiscoveryPipeline) {
         try {
             // Try to find the jar inside the jar
             var jarFilePath = file.findResource(path);
@@ -77,7 +76,9 @@ public class NeoForgeJarInJarLoader extends AbstractJarFileDependencyLocator {
             );
 
             // Create a new ModFile for it
-            return Optional.of(createMod(jarZipFS.getPath("/")).file());
+            var jar = JarContents.of(List.of(jarFilePath));
+            var providerResult = iDiscoveryPipeline.readModFile(jar, ModFileDiscoveryAttributes.DEFAULT.withParent(file));
+            return Optional.ofNullable(providerResult);
         } catch (Exception e) {
             return Optional.empty();
         }
@@ -96,15 +97,5 @@ public class NeoForgeJarInJarLoader extends AbstractJarFileDependencyLocator {
         }
 
         return Optional.empty();
-    }
-
-    @Override
-    public String name() {
-        return "neo-jarinjar";
-    }
-
-    @Override
-    public void initArguments(Map<String, ?> arguments) {
-
     }
 }
